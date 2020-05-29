8b248420d0c0ab94badb9566a6708583b34508a4
mukul34 <sirohi.mukul1999@gmail.com>
2020-05-29 16:59:09 +0530

    done

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 const api = {
   key: "0072c6c507246e6a2f8dbe368ba52673",
-  base: "http://api.openweathermap.org/data/2.5/",
+  base: "https://api.openweathermap.org/data/2.5/",
 };
 
 function App() {
